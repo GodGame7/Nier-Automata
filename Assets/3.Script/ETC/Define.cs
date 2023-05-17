68d55b49d8ab5a4c90cff46d897fcfbe0bafddd6
GamJia <123979439+Latte95@users.noreply.github.com>
2023-05-17 11:10:07 +0900

    Fix_AudioManager.cs_기본세팅
    BGM & SFX 재생을 위한 기능 추가

---
+++ Assets/3.Script/ETC/Define.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define : MonoBehaviour
+{
+    public enum BGM
+    {
+        Flag, MainField,
+    }
+
+    public enum SFX
+    {
+        BtnClick, BtnOver, //플레이어 효과음 이름 추가
+    }
+}
