f6d15636daefcc173a971b24848c0fb2527a44c9
GamJia <123979439+Latte95@users.noreply.github.com>
2023-05-17 17:43:05 +0900

    Fit_FlagControl.cs_공격 틀 구현(기능x)
    약공격, 강공격은 서로 실행중에 재실행되지 않아야 하기 때문에 상태패턴으로 구현함

--- Assets/3.Script/Player/Flag/FlagControl.cs
+++ Assets/3.Script/Player/Flag/FlagControl.cs
@@ -11,14 +11,26 @@ public class FlagControl : MonoBehaviour
     private float turnSpeed = 2.7f;
     [SerializeField]
     private float fireDelay = 0.1f;
+    [SerializeField]
+    private float damage = 5.0f;
+    private float weakDamage = 2.0f;
+    private float strong1Damage = 3.0f;
+    private float strong2Damage = 3.1f;
 
 
     // 플레이어
-    private float _speed;
-    private float _animationBlend;
-    private float _targetRotation = 0.0f;
+    //private float animationBlend;
+    //private float targetRotation = 0.0f;
+    //private PlayerData player;
 
-    private IFlagMoveStrategy _currentStrategy;
+
+    private IFlagMoveStrategy currentStrategy;
+    private IFlagState currentState;
+    private FlagNomal nomalState;
+    private FlagWeakAttack weakAttackState;
+    private FlagStrongAttack1 strongAttackState1;
+    private FlagStrongAttack2 strongAttackState2;
+    private bool isStrongAttackCombo = false;
 
     // 애니매이션 해시
     private int hashHSpeed;
@@ -34,6 +46,11 @@ public class FlagControl : MonoBehaviour
 
     private const float _threshold = 0.01f;
 
+    // 캐싱
+    private WaitUntil InputWeakAttackButton_wait;
+    private WaitUntil InputStrongAttackButton_wait;
+    private WaitUntil InputFireButton_wait;
+    private WaitForSeconds FireDelay_wait;
 
     private void Awake()
     {
@@ -44,6 +61,29 @@ public class FlagControl : MonoBehaviour
 
         Init();
         GetAnimHash();
+
+        nomalState = new FlagNomal();
+        weakAttackState = new FlagWeakAttack();
+        strongAttackState1 = new FlagStrongAttack1();
+        strongAttackState2 = new FlagStrongAttack2();
+
+        currentState = nomalState;
+    }
+
+    private void OnEnable()
+    {
+        // 이벤트 구독 (시점 변환)
+        StartCoroutine(nameof(WeakAttack_co));
+        StartCoroutine(nameof(StrongAttack_co));
+        StartCoroutine(nameof(Fire_co));
+    }
+
+    private void OnDisable()
+    {
+        // 이벤트 해제 (시점 변환)
+        StopCoroutine(nameof(WeakAttack_co));
+        StopCoroutine(nameof(StrongAttack_co));
+        StopCoroutine(nameof(Fire_co));
     }
 
     private void Start()
@@ -51,6 +91,7 @@ public class FlagControl : MonoBehaviour
         SetStrategy(new TopViewFlagMove());
         SetStrategy(new SideViewFlagMove());
         SetStrategy(new BackViewFlagMove());
+        SetState(nomalState);
     }
 
     private void Update()
@@ -62,6 +103,11 @@ public class FlagControl : MonoBehaviour
     {
         TryGetComponent(out anim);
         TryGetComponent(out controller);
+
+        InputWeakAttackButton_wait = new WaitUntil(()=>Input.GetKey(KeyCode.Period) || Input.GetMouseButton(0));
+        InputStrongAttackButton_wait = new WaitUntil(() => Input.GetKey(KeyCode.Slash) || Input.GetMouseButton(1));
+        InputFireButton_wait = new WaitUntil(() => Input.GetKey(KeyCode.LeftShift));
+        FireDelay_wait = new WaitForSeconds(fireDelay);
     }
 
     private void GetAnimHash()
@@ -75,16 +121,81 @@ public class FlagControl : MonoBehaviour
 
     public void SetStrategy(IFlagMoveStrategy strategy)
     {
-        _currentStrategy = strategy;
+        currentStrategy = strategy;
+    }
+    private void SetState(IFlagState state)
+    {
+        currentState = state;
+    }
+    public void ExecuteAttack()
+    {
+        currentState.Action();
     }
 
     private void Move()
     {
         Vector3 move;
 
-        _currentStrategy.Move(this, out move);
+        currentStrategy.Move(this, out move);
 
         anim.SetFloat(hashHSpeed, move.x);
         controller.Move(moveSpeed * Time.deltaTime * move);
+    }
+
+    private IEnumerator WeakAttack_co()
+    {
+        while (true)
+        {
+            yield return InputWeakAttackButton_wait;
+            if (currentState is FlagNomal)
+            {
+                SetState(weakAttackState);
+                ExecuteAttack();
+                StartCoroutine(ReturnToNomalState_co(new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99f && anim.GetCurrentAnimatorStateInfo(0).IsName("WeakAttack"))));
+            }
+        }
+    }
+    private IEnumerator StrongAttack_co()
+    {
+        while (true)
+        {
+            yield return InputStrongAttackButton_wait;
+
+            if (currentState is FlagNomal)
+            {
+                if (!isStrongAttackCombo)
+                {
+                    SetState(strongAttackState1);
+                }
+                else
+                {
+                    SetState(strongAttackState2);
+                }
+                ExecuteAttack();
+                StartCoroutine(ReturnToNomalState_co(new WaitUntil(()=>anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.99f && (anim.GetCurrentAnimatorStateInfo(0).IsName("StrongAttack1")|| anim.GetCurrentAnimatorStateInfo(0).IsName("StrongAttack2")))));
+            }
+            else if (currentState is FlagStrongAttack1)
+            {
+                SetState(strongAttackState2);
+            }
+
+            
+        }
+    }
+    private IEnumerator Fire_co()
+    {
+        while (true)
+        {
+            yield return FireDelay_wait;
+            yield return InputFireButton_wait;
+
+            Debug.Log("발사");
+        }
+    }
+    private IEnumerator ReturnToNomalState_co(WaitUntil waitAnimationEnd)
+    {
+        //yield return waitAnimationEnd;
+        yield return null;
+        SetState(nomalState);
     }
 }
