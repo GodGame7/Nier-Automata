7318f8a7e16079c53accce3d922ed151b277f715
HaloTwo <lsm6265@naver.com>
2023-05-18 17:24:54 +0900

    Fix_Enemy0000
    첫번째 쪼꼬미 적, 상태패턴으로 구현했습니다.
    hp도 파라메타로 넣었고, 이제 데미지 받는것만 구현하면 됩니다.

--- Assets/3.Script/Enemy/em0000.cs
+++ Assets/3.Script/Enemy/em0000.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,94 +14,88 @@ public class em0000 : MonoBehaviour
         JUMP,
         DASH,
         ATTACK,
-        DIE,
+        DIE
     }
 
-    float movespeed = 1f;
+    [SerializeField] private float Hp;
+    public float hp
+    {
+        get { return Hp; }
+        set
+        {
+            Hp = value;
+
+            if (Hp <= 0)
+            {
+                StopCoroutine(CheckState());
+                isdead = true;
+                anim.SetTrigger("Die");
+                state = State.DIE;
+            }
+        }
+    }
+
+    //타겟과의 거리
+    float distance;
     bool isdead = false;
 
-    //목적지
-    public Transform target;
+    [SerializeField] BoxCollider[] boxCollider;
 
-    //요원
-    NavMeshAgent agent;
+    //타겟
+    [SerializeField] Transform target;
+
     Animator anim;
 
-    [SerializeField]State state;
+    //상태
+    [SerializeField] State state;
+
+    private void Awake()
+    {
+        TryGetComponent(out anim);
+        boxCollider = GetComponentsInChildren<BoxCollider>();
+    }   
+
 
     private void Start()
     {
-
+        //타겟 위치 찾기
+        target = GameObject.FindGameObjectWithTag("Player").transform;
         state = State.IDLE;
 
-        TryGetComponent(out anim);
-        TryGetComponent(out agent);
-
-        target = GameObject.Find("Player").transform;
-        //요원에게 목적지를 알려준다.
+        //콜라이더 끄기 => 근데 안됌
+        //for (int i = 0; i < boxCollider.Length; i++)
+        //{
+        //    boxCollider[i].enabled = false;
+        //}
 
         StartCoroutine(CheckState());
     }
 
-
-    void Walk()
-    {
-        anim.SetBool("Walk", true);
-        //남은 거리가 2미터라면 공격한다.
-
-        //타겟 방향으로 이동하다가
-        //agent.speed = 0.1f;
-        float distance = Vector3.Distance(transform.position, target.transform.position);
-        Debug.Log(distance);
-
-
-        if (distance <= 0.5f)
-        {
-            state = State.ATTACK;
-        }
-        else if(distance > 2f)
-        {
-            state = State.DASH;
-        }
-    }
-
-
-    void Idle()
-    {
-        state = State.WALK;
-    }
-
-    void Attack()
-    {
-        anim.SetTrigger("Attack2");
-        
-        if (true)
-        {
-
-        }
-    }
-
-    void UpdateDash()
-    {
-        anim.SetTrigger("Dash");
-    }
-
+    Stopwatch sw = new Stopwatch();
     IEnumerator CheckState()
     {
         while (!isdead)
         {
-            yield return new WaitForSeconds(0.2f);
+            // 타겟 위치를 얻어온다.
+            Vector3 targetPosition = target.position;
+            targetPosition.y = 0f;
 
-            agent.destination = target.transform.position;
+            // 타겟 방향을 바라보도록 회전
+            transform.LookAt(targetPosition);
+
+            //타겟과 거리를 측정
+            distance = Vector3.Distance(transform.position, target.transform.position);
 
             switch (state)
             {
                 case State.IDLE:
-                    Idle();
+                    //1초 뒤에 걷기 시작
+                    yield return new WaitForSeconds(1f);
+                    state = State.WALK;
                     break;
 
                 case State.WALK:
-                    Walk();
+                    UpdateWalk();
                     break;
 
                 case State.JUMP:
@@ -114,13 +109,85 @@ public class em0000 : MonoBehaviour
                     Attack();
                     break;
 
-                case State.DIE:
-                    break;
-
-                default:
-                    break;
             }
+            //AnimatorStateInfo aniInfo = anim.GetCurrentAnimatorStateInfo(0);
+            //if ( (aniInfo.IsName("em0000_Attack End(Windmill)") && aniInfo.normalizedTime >= 1) ||
+            //    (aniInfo.IsName("em0000_Attack") && aniInfo.normalizedTime >= 1))
+            //{
+            //    boxCollider.enabled = false;
+            //}
+            yield return null;
         }
+
+    }
+
+    void UpdateWalk()
+    {
+        //걷기
+        anim.SetBool("Walk", true);
+
+        //남은 거리가 3f 이하면 공격하고, 6f 이상이면 대쉬함
+        if (distance < 2.8f)
+        {
+            state = State.ATTACK;
+        }
+        else if (distance >= 6f)
+        {
+            state = State.DASH;
+        }
+    }
+
+    void Attack()
+    {
+        sw.Start();
+        int random = Random.Range(1, 3);
+        UnityEngine.Debug.Log(random);
+
+        switch (random)
+        {
+            case 1:
+                WindmillAttack();
+                break;
+            case 2:
+                PunchAttack();
+                break;
+        }
+    }
+
+    void WindmillAttack()
+    {
+        if (!anim.GetBool("Attack2"))
+        {
+            anim.SetBool("Attack2", true);
+        }
+        else if (anim.GetBool("Attack2") && (distance <= 2f || sw.ElapsedMilliseconds > 4000.0f))
+        {
+            anim.SetBool("Attack2", false);
+            state = State.IDLE;
+            sw.Reset();
+        }
+
+    }
+
+    void PunchAttack()
+    {
+        if (!anim.GetBool("Attack2"))
+        {
+            anim.SetTrigger("Attack1");
+            state = State.IDLE;
+            sw.Reset();
+        }
+    }
+
+    void UpdateDash()
+    {
+        anim.SetTrigger("Dash");
+        state = State.WALK;
+    }
+
+
+    public void TakeDamage(float damage)
+    {
 
     }
 }
