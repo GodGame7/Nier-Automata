2ed65c7d9caa295ec2dd08b87655bc42250f1ff7
GamJia <123979439+Latte95@users.noreply.github.com>
2023-05-19 15:44:06 +0900

    Fix_BulletSpawner_오브젝트 풀링 위한 총알 생성

---
+++ Assets/3.Script/Player/Flag/Bullet/BulletSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet_pref;
+    private GameObject[] bullets;
+    private int bulletNum = 20;
+
+    private void Awake()
+    {
+        for(int i = 0; i< bulletNum; i++)
+        {
+            bullets[i] = Instantiate(bullet_pref, transform);
+            bullets[i].SetActive(false);
+        }
+    }
+    public void Fire()
+    {
+
+    }
+}
