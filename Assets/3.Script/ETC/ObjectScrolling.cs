e240baca001e399246e1e97b34fbddd48d09ad93
kingkong24 <129942986+kingkong24@users.noreply.github.com>
2023-05-19 11:56:00 +0900

    FlagFight씬 오브젝트 스크롤링, DestroyCoilder 등 구현

---
+++ Assets/3.Script/ETC/ObjectScrolling.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScrolling : MonoBehaviour
+{
+    [SerializeField] float scrollingSpeed;
+    [SerializeField] float ZScale;
+    [SerializeField] float RepositionRatio;
+
+    Vector3 offsetPosition;
+    float repotision;
+
+    private void Awake()
+    {
+        offsetPosition = new Vector3(0, 0, 3f * ZScale);
+        repotision = -RepositionRatio * ZScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z <= repotision)
+        {
+            Reposition();
+        }
+        transform.Translate(Vector3.back * scrollingSpeed * Time.deltaTime);
+    }
+
+    void Reposition()
+    {
+        transform.position = transform.position + offsetPosition;
+    }
+}
---
+++ Assets/3.Script/Manager/FlagFightManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagFightManager : MonoBehaviour
+{
+    // 여기에서 FlagFight 씬의 각종 메소드의 발동 타이밍을 정해줍니다.
+
+}
