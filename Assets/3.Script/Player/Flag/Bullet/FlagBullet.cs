61ece997891bf64ce110bd770397c3678c53a1e7
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-05-22 16:43:49 +0900

    Comment_FlagBullet_이후 변경해야 될 내용 주석으로 메모

--- Assets/3.Script/Player/Flag/Bullet/FlagBullet.cs
+++ Assets/3.Script/Player/Flag/Bullet/FlagBullet.cs
@@ -18,12 +18,14 @@ public class FlagBullet : MonoBehaviour
         {
             if(TryGetComponent(out Em0032Movement enemy))
             {
+                // todo 데미지 플레이어 데이터 받아와서 설정할 것
                 enemy.OnDamage(1);
             }
             gameObject.SetActive(false);
         }
         else if(other.CompareTag("BulletSoft"))
         {
+            // todo 적 총알 비활성화인지 파괴인지
             other.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
