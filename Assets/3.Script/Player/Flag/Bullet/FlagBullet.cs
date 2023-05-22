93b0399ddfbca4eeb35bf6378b42f4bc6156332f
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-05-22 16:42:09 +0900

    Fix_FlagBullet_총알로 적 타격 & 일반 총알 파괴

--- Assets/3.Script/Player/Flag/Bullet/FlagBullet.cs
+++ Assets/3.Script/Player/Flag/Bullet/FlagBullet.cs
@@ -11,4 +11,21 @@ public class FlagBullet : MonoBehaviour
     {
         transform.position += moveSpeed * Time.deltaTime * Vector3.forward;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            if(TryGetComponent(out Em0032Movement enemy))
+            {
+                enemy.OnDamage(1);
+            }
+            gameObject.SetActive(false);
+        }
+        else if(other.CompareTag("BulletSoft"))
+        {
+            other.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
 }
