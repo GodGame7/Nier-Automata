29160407884c6ba22373b2701ff33bcd974a9876
VillageManA <sjthals@naver.com>
2023-05-22 15:43:30 +0900

    InevenUI 주석 변경 (싱글톤선언이후 교체)

--- Assets/3.Script/Inventory/InventoryUI.cs
+++ Assets/3.Script/Inventory/InventoryUI.cs
@@ -7,16 +7,24 @@ public class InventoryUI : MonoBehaviour
 {
     [SerializeField] Text[] Slot;
     [SerializeField] ItemData ItemData;
-
+    private int InvenLength;
+    private void Awake()
+    {
+        //InvenLength = Player.instance.inventory.count;
+    }
     public void UpdateUI()
     {
-        for (int i = 0; i < Slot.Length; i++) // 플레이어 아이템 리스트 Count까지 해서 바꿔주렴 
-                
+        for (int i = 0; i < Slot.Length / 2; i++) // InvenLength까지  //아이템 이름
+
         {
-            Slot[i].text = string.Format("{0} , {1}", ItemData.ItemName, ItemData.Quantity); // ItemData를 Player.instacne.inventory[i]로 변경
+            Slot[i].text = string.Format("{0}", ItemData.ItemName); // ItemData를 Player.instacne.inventory[i]로 변경
             Slot[i].color = Color.white;
         }
+        for (int i = Slot.Length / 2; i < Slot.Length; i++) // 아이템 수량
+        {
+            Slot[i].text = string.Format("{0}", ItemData.Quantity); // player.instance.inventory[i-InvenLength] 로 변경
+        }
         //툴팁을 아이템 갯수로 바꿔줄예정
-        
+
     }
 }
