ca62596cfc0066033300b498b3b577ee7d85a574
HaloTwo <lsm6265@naver.com>
2023-05-23 18:18:37 +0900

    Fix_Enemy
    다 코루틴으로 바꿈

--- Assets/3.Script/Enemy/em0010.cs
+++ Assets/3.Script/Enemy/em0010.cs
@@ -14,29 +14,15 @@ public class em0010 : Enemy
     {
         while (!isdead)
         {
-            AnimatorStateInfo animStateInfo = anim.GetCurrentAnimatorStateInfo(0);
-            animatorinfo = this.anim.GetCurrentAnimatorClipInfo(0);
-            string current_animation = animatorinfo[0].clip.name;
-
             //거리확인
             Distance();
 
-            //공격 모션이 나올땐, 공격모션만 실행
-            if (current_animation.Contains("Attack") ||
-                current_animation.Contains("Hit") ||
-                current_animation.Contains("Die"))
-            {
-                yield return null;
-                continue;
-            }
-
-            //타겟을 바라보는 메서드
             TargetLookat();
 
             switch (state)
             {
                 case State.IDLE:
-                    UpdateIdle();               
+                    yield return StartCoroutine(UpdateIdle2());
                     break;
 
                 case State.WALK:
@@ -45,7 +31,7 @@ public class em0010 : Enemy
 
                 case State.ATTACK:
                     //패턴의 갯수만큼에서 랜덤공격
-                    UpdateAttack(pattonNum);
+                    yield return StartCoroutine(UpdateAttack2(pattonNum));
                     break;
             }
             yield return null;
