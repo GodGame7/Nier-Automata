f0a031d9c3f15a1eee9a74caa884cdc23fac12c7
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-05-31 13:40:05 +0900

    Refactor_FollowUI.cs_메인카메라 캐싱

--- Assets/3.Script/UI/FollowUI.cs
+++ Assets/3.Script/UI/FollowUI.cs
@@ -6,22 +6,24 @@ public class FollowUI : MonoBehaviour
 {
     public GameObject target_obj;
     private RectTransform UITrans;
+    private Camera mainCamera;
 
     private Vector3 distance = new Vector3(-200, 150, 0);
 
     private void Awake()
     {
         TryGetComponent(out UITrans);
+        mainCamera = Camera.main;
     }
 
     private void LateUpdate()
     {
         if (!target_obj.activeSelf)
         {
-            gameObject.SetActive(false);
+            Destroy(gameObject);
             return;
         }
-        Vector3 ScreenPosition = Camera.main.WorldToScreenPoint(target_obj.transform.position);
+        Vector3 ScreenPosition = mainCamera.WorldToScreenPoint(target_obj.transform.position);
 
         UITrans.position = ScreenPosition + distance;
     }
