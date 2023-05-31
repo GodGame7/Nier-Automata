e07f2ab457fa0f0673135d00539d4000af42b8d1
Dani Kang <90600217+kdi2344@users.noreply.github.com>
2023-05-31 09:43:16 +0900

    Merge_MainPlayer_플레이어기능구현
    이동 대쉬 공격
    --
    추후 작업
    상호작용(공격피격)
    닷지 기능구현 및 닷지 효과
    애니메이션 다듬기(무기 피벗)

---
+++ Assets/3.Script/Main_Player/State/State_Atk.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_Atk : State
+{
+    float lastAtkTime;
+    float strongCount;
+    StateManager sm;
+    bool isStrongAtk;
+    public override void Enter(State before)
+    {
+        isStrongAtk = false;
+        Main_Player.Instance.isAtk = true;
+        Main_Player.Instance.anim_player.applyRootMotion = true;
+        Attack1();
+    }
+
+    public override void Exit(State next)
+    {
+        Main_Player.Instance.anim_player.applyRootMotion = false;
+        Main_Player.Instance.anim_sword.SetTrigger("Idle");
+        Main_Player.Instance.anim_bigsword.SetTrigger("Idle");
+
+    }
+
+    public override void StateFixedUpdate()
+    {
+
+    }
+
+    public override void StateUpdate()
+    {
+        Main_Player.Instance.Rotate();
+    }
+
+    //private IEnumerator StrongAttack_co()
+    //{
+    //    isStrongAtk = true;
+    //    Main_Player.Instance.anim_player.SetTrigger("AtkStrong");
+    //    yield return new WaitForSeconds(2f);
+    //    Main_Player.Instance.isAtk = false;
+    //}
+    //private void StrongAttack()
+    //{
+    //    StartCoroutine(StrongAttack_co());
+    //}
+    private IEnumerator Attack_co()
+    {
+        lastAtkTime = Time.time;
+        Main_Player.Instance.anim_player.SetTrigger("Atk1");
+        Main_Player.Instance.anim_sword.SetTrigger("Atk1");
+
+        yield return AtkListener(2);
+        yield return CancleListener();
+        Main_Player.Instance.isAtk = false;
+    }
+
+    private IEnumerator CancleListener()
+    {
+        yield return new WaitForEndOfFrame();
+        float count = 0f;
+        while (count < 2f)
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            {
+                break;
+            }
+            count += Time.deltaTime;
+            yield return null;
+        }
+    }
+    private IEnumerator AtkListener(int i)
+    {
+        yield return new WaitForEndOfFrame();
+        if (i > 5)
+        {
+            yield return new WaitForSeconds(2f);
+            yield break;
+        }
+        float count = 0f;
+        while (count < 2f)
+        {
+            if (!isStrongAtk && Input.GetMouseButtonDown(0))
+            {
+                lastAtkTime = Time.time;
+                Main_Player.Instance.anim_player.SetTrigger("Atk"+i);
+                Main_Player.Instance.anim_sword.SetTrigger("Atk" + i);
+                yield return AtkListener(i+1);
+                break;
+            }
+            count += Time.deltaTime;
+            yield return null;
+        }
+        yield break;
+    }
+    public void Attack1()
+    {
+        StartCoroutine(Attack_co());
+    }
+}
---
+++ Assets/3.Script/Main_Player/State/State_DashBack.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State_DashBack : State
+{
+    StateManager sm;
+    float lastdashtime = 0;
+    float dashbat = 0.5f;
+    private void Awake()
+    {
+        sm = FindObjectOfType<StateManager>();
+    }
+    public override void Enter(State before)
+    {
+        Main_Player.Instance.isDash = true;
+        lastdashtime = Time.time;
+        Dash();
+    }
+    public override void Exit(State next)
+    {
+
+    }
+    public override void StateFixedUpdate()
+    {
+
+    }
+    public override void StateUpdate()
+    {
+        
+    }
+    private IEnumerator DashBack()
+    {
+        Main_Player.Instance.anim_player.SetTrigger("DashBack");
+        Main_Player.Instance.isDash = true;
+        while (Time.time - lastdashtime < dashbat)
+        {
+            transform.Translate(Vector3.back * 15f * Time.deltaTime);
+            if (Time.time - lastdashtime < 0.3f)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Debug.Log("´åÁö");
+                }
+            }
+            yield return null;
+        }
+        Main_Player.Instance.anim_player.SetTrigger("DashEnd");
+        yield return new WaitForSeconds(0.4f);
+        Main_Player.Instance.isDash = false;
+    }
+
+    private void Dash()
+    {
+            StartCoroutine(DashBack());
+    }
+
+
+}
