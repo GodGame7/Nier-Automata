1037d4433906c674a905fc62bc20ec6201cf51b5
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-06-01 13:21:20 +0900

    Comment_FlagControl.cs_주석 추가

--- Assets/3.Script/Player/Flag/FlagControl.cs
+++ Assets/3.Script/Player/Flag/FlagControl.cs
@@ -76,18 +76,18 @@ public class FlagControl : MonoBehaviour
     // 캐싱
     #region 코루틴 Wait
     public WaitUntil InputFireButton_wait { get; private set; }
+    public WaitUntil ResetCombo_wait { get; private set; }
     public WaitUntil EnterDashAni_wait { get; private set; }
     public WaitUntil ExitDashAni_wait { get; private set; }
     public WaitUntil EnterAttackAni_wait { get; private set; }
     public WaitUntil ExitAttackAni_wait { get; private set; }
-    public WaitUntil ResetCombo_wait { get; private set; }
     public WaitUntil EnterTransformation_wait { get; private set; }
     public WaitUntil ExitTransformation_wait { get; private set; }
+    public WaitUntil EnterGundamAttack1_wait { get; private set; }
 
     public WaitForSeconds FireDelay_wait { get; private set; }
     public WaitForSeconds AnimaReset_wait { get; private set; }
     #endregion 코루틴 Wait
-
 
     #region 초기화
     private void Awake()
@@ -129,7 +129,7 @@ public class FlagControl : MonoBehaviour
         TryGetComponent(out rigid);
         bulletSpawners = GetComponentsInChildren<FlagBulletSpawner>();
 
-        InputFireButton_wait = new WaitUntil(() => Input.GetKey(KeyCode.LeftShift));
+        InputFireButton_wait = new WaitUntil(() => Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
         EnterDashAni_wait = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("FlagDash") || anim.GetCurrentAnimatorStateInfo(0).IsName("GundamDash"));
         ExitDashAni_wait = new WaitUntil(() => !(anim.GetCurrentAnimatorStateInfo(0).IsName("FlagDash") || anim.GetCurrentAnimatorStateInfo(0).IsName("GundamDash")));
         EnterAttackAni_wait = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("HorizontalWeakAttack") || anim.GetCurrentAnimatorStateInfo(0).IsName("VerticalWeakAttack") || anim.GetCurrentAnimatorStateInfo(0).IsName("FlagStrongAttack") ||
@@ -138,6 +138,7 @@ public class FlagControl : MonoBehaviour
                                                   anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack1") || anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack2") || anim.GetCurrentAnimatorStateInfo(0).IsName("GundamStrongAttack")));
         EnterTransformation_wait = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("ToFlag") || anim.GetCurrentAnimatorStateInfo(0).IsName("ToGundam"));
         ExitTransformation_wait = new WaitUntil(() => !(anim.GetCurrentAnimatorStateInfo(0).IsName("ToFlag") || anim.GetCurrentAnimatorStateInfo(0).IsName("ToGundam")));
+        EnterGundamAttack1_wait = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack1"));
         FireDelay_wait = new WaitForSeconds(fireDelay);
         AnimaReset_wait = new WaitForSeconds(preInputDelay);
         ResetCombo_wait = new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack1") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.98f);
@@ -169,6 +170,7 @@ public class FlagControl : MonoBehaviour
     #endregion 초기화
 
     #region 전략, 상태
+    // 시점
     public void SetTopViewStrategy()
     {
         SetViewStrategy(new FlagTopViewMove());
@@ -187,6 +189,8 @@ public class FlagControl : MonoBehaviour
         StartCoroutine(MoveToCenter(0.5f));
         currentViewStrategy = strategy;
     }
+
+    // 모드
     public void TransGundam()
     {
         SetModeStrategy(new ModeGundam());
@@ -209,22 +213,17 @@ public class FlagControl : MonoBehaviour
 
         currentModeStrategy = strategy;
     }
+
+    // 상태
     private void SetState(IFlagState state)
     {
         StopCoroutine(nameof(ReturnToNomalState_co));
         currentState = state;
     }
-    public IEnumerator ReturnToNomalState_co(WaitUntil startAnimation = null, WaitUntil endAnimation = null)
+    public IEnumerator ReturnToNomalState_co(WaitUntil wait1 = null, WaitUntil wait2 = null)
     {
-        yield return startAnimation;
-        yield return null;
-        yield return endAnimation;
-
-        SetState(nomalState);
-    }
-    public IEnumerator ReturnToNomalState_co(WaitForSeconds wait)
-    {
-        yield return wait;
+        yield return wait1; // 애니메이션 시작, 키입력 등 
+        yield return wait2; // 애니메이션 완료, null 등
 
         SetState(nomalState);
     }
@@ -233,9 +232,11 @@ public class FlagControl : MonoBehaviour
         while (true)
         {
             yield return EnterTransformation_wait;
+            // 모드 변환 애니메이션 실행되면 플레이어 회전값 초기화 (건담 모드에서 돌아가기 때문에)
             transform.rotation = Quaternion.Euler(-90f, 0, 0);
             SetState(transfomationState);
             yield return ExitTransformation_wait;
+            // 모드 변환 완료 시 노말 상태로 복귀
             SetState(nomalState);
         }
     }
@@ -259,10 +260,7 @@ public class FlagControl : MonoBehaviour
     {
         if (!currentState.Equals(transfomationState) && canMove)
         {
-            if (currentModeStrategy is ModeGundam)
-            {
-                currentModeStrategy.Rotate(this);
-            }
+            currentModeStrategy.Rotate(this);
             Move();
         }
     }
@@ -270,6 +268,8 @@ public class FlagControl : MonoBehaviour
     #region 이동
     private void InputMoveKey()
     {
+        // 키입력에 따라 이동 방향 결정
+        // 방향만 결정하는 이유는 시점에 따라 이동할 방향이 달라 이동 자체는 다른 곳에서 처리하기 때문
         if (Input.GetKey(KeyCode.A))
         {
             h = -1;
@@ -297,13 +297,15 @@ public class FlagControl : MonoBehaviour
     }
     private void Move()
     {
-        float targetSpeed = moveSpeed;
-        Vector3 move;
-        currentViewStrategy.Move(this, out move);
+        float targetSpeed = moveSpeed;  // 대쉬 속도 등 모두 계산된 최종 이동속도
+        Vector3 moveDir;
+        // 시점에 따른 이동 방향 결정
+        currentViewStrategy.Move(this, out moveDir);    
 
+        // 이동 방향에 따른 애니메이션 설정
         if (currentModeStrategy is ModeFlag)
         {
-            animationBlend = Mathf.Lerp(animationBlend, move.x, Time.deltaTime * speedChangeRate);
+            animationBlend = Mathf.Lerp(animationBlend, moveDir.x, Time.deltaTime * speedChangeRate);
             if (Mathf.Abs(animationBlend) < 0.01f)
             {
                 animationBlend = 0f;
@@ -312,13 +314,13 @@ public class FlagControl : MonoBehaviour
         }
         else
         {
-            if (Mathf.Abs(move.x) > Mathf.Abs(move.z))
+            if (Mathf.Abs(moveDir.x) > Mathf.Abs(moveDir.z))
             {
-                animationBlend = Mathf.Lerp(animationBlend, Mathf.Abs(move.x), Time.deltaTime * speedChangeRate);
+                animationBlend = Mathf.Lerp(animationBlend, Mathf.Abs(moveDir.x), Time.deltaTime * speedChangeRate);
             }
             else
             {
-                animationBlend = Mathf.Lerp(animationBlend, Mathf.Abs(move.z), Time.deltaTime * speedChangeRate);
+                animationBlend = Mathf.Lerp(animationBlend, Mathf.Abs(moveDir.z), Time.deltaTime * speedChangeRate);
             }
             if (animationBlend < 0.01f)
             {
@@ -327,12 +329,15 @@ public class FlagControl : MonoBehaviour
             anim.SetFloat(hashAniSpeed, animationBlend);
         }
 
-
+        // 플레이어 이동
         if (currentState is FlagDash)
         {
             targetSpeed *= dashSpeed;
         }
-        Vector3 newPosition = new Vector3(Mathf.Clamp((rigid.position.x + targetSpeed * Time.deltaTime * move.x), -0.3f, 0.3f), Mathf.Clamp((rigid.position.y + targetSpeed * Time.deltaTime * move.y), -0.18f, 0.18f), Mathf.Clamp((rigid.position.z + targetSpeed * Time.deltaTime * move.z), -0.15f, 0.15f));
+        Vector3 newPosition = new Vector3(
+            Mathf.Clamp((rigid.position.x + targetSpeed * Time.deltaTime * moveDir.x), -0.3f, 0.3f),
+            Mathf.Clamp((rigid.position.y + targetSpeed * Time.deltaTime * moveDir.y), -0.18f, 0.18f),
+            Mathf.Clamp((rigid.position.z + targetSpeed * Time.deltaTime * moveDir.z), -0.15f, 0.15f));
         rigid.MovePosition(newPosition);
     }
     private void CheckDash()
@@ -359,25 +364,26 @@ public class FlagControl : MonoBehaviour
     }
     public IEnumerator ResetScaleX_co()
     {
-        yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("FlagDash"));
-        yield return null;
-        yield return new WaitUntil(() => !anim.GetCurrentAnimatorStateInfo(0).IsName("FlagDash"));
+        yield return EnterDashAni_wait;
+        yield return ExitDashAni_wait;
         transform.localScale = Vector3.one;
     }
 
+    // 시점변경 등 플레이어 중앙으로 강제 이동시키는 메소드
     public IEnumerator MoveToCenter(float speed)
     {
         canMove = false;
 
-        while (Vector3.SqrMagnitude(transform.position - defaultPos) >= 0.0004f)
+        while (Vector3.SqrMagnitude(transform.position - defaultPos) >= 0.0001f)
         {
-            Vector3 direction = (defaultPos - transform.position).normalized; // 방향 벡터 계산
-            Vector3 moveVector = direction * speed * Time.deltaTime; // 이동 벡터 계산
+            Vector3 direction = (defaultPos - transform.position).normalized;   // 이동해야 할 방향 계산
+            Vector3 moveVector = direction * speed * Time.deltaTime;            // 속도를 고려해 이동 해야할 위치 계산
 
             rigid.MovePosition(transform.position + moveVector);
             yield return null;
         }
         transform.position = defaultPos;
+
         canMove = true;
     }
     public void StopMove()
@@ -397,6 +403,7 @@ public class FlagControl : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Period) || Input.GetMouseButtonDown(0))
             {
+                // 비행기 모드의 약공격이 시점에 따라 수직오르 돌지 수평으로 돌지 결정하기 위한 변수
                 bool isHorizontal;
                 if (currentViewStrategy is FlagSideViewMove)
                 {
@@ -406,8 +413,9 @@ public class FlagControl : MonoBehaviour
                 {
                     isHorizontal = true;
                 }
-                SetState(attackState);
+
                 currentModeStrategy.WeakAttack(this, isHorizontal);
+                SetState(attackState);
                 StopCoroutine(nameof(ReturnToNomalState_co));
                 StartCoroutine(ReturnToNomalState_co(EnterAttackAni_wait, ExitAttackAni_wait));
             }
@@ -419,6 +427,7 @@ public class FlagControl : MonoBehaviour
                 StartCoroutine(ReturnToNomalState_co(EnterAttackAni_wait, ExitAttackAni_wait));
             }
         }
+        // 건담모드는 공격중에도 약공격 콤보로 발동 가능
         else if (currentModeStrategy is ModeGundam)
         {
             if (Input.GetKeyDown(KeyCode.Period) || Input.GetMouseButtonDown(0))
@@ -449,9 +458,11 @@ public class FlagControl : MonoBehaviour
         float startTime;
         while (true)
         {
-            yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack1"));
+            // 건담 1단 공격
+            yield return EnterGundamAttack1_wait;
             isCombo = true;
             startTime = Time.time;
+            // 2단 공격이 실행되거나 1단 공격이 실행 된 지 오래 됐으면 콤보 리셋
             yield return new WaitUntil(() => (Time.time >= startTime + waitTime) || anim.GetCurrentAnimatorStateInfo(0).IsName("GundamWeakAttack2"));
             isCombo = false;
         }
@@ -460,6 +471,7 @@ public class FlagControl : MonoBehaviour
 
     public void SetAnimaTrigger(int hashAni)
     {
+        // 트리거가 발동되기 전까지 On되있지 않고 몇초 뒤 꺼지도록 하기 위한 메소드
         anim.SetTrigger(hashAni);
         StopCoroutine(nameof(ResetAnimaTrigger_co));
         StartCoroutine(nameof(ResetAnimaTrigger_co), hashAni);
