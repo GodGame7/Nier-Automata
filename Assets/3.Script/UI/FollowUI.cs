2e6b7144187176d5d8c28443a13fa2bf99a987a8
Latte95 <123979439+Latte95@users.noreply.github.com>
2023-06-05 15:41:04 +0900

    Fix_FollowUI.cs_UI On/Off 추가

--- Assets/3.Script/UI/FollowUI.cs
+++ Assets/3.Script/UI/FollowUI.cs
@@ -7,14 +7,26 @@ public class FollowUI : MonoBehaviour
     public GameObject target_obj;
     private RectTransform UITrans;
     private Camera mainCamera;
+    private Animator anim;
 
     private Vector3 distance = new Vector3(-200, 150, 0);
+
+    private int hashPopUp;
 
     private void Awake()
     {
         TryGetComponent(out UITrans);
+        TryGetComponent(out anim);
         mainCamera = Camera.main;
         target_obj = GameObject.FindGameObjectWithTag("Player");
+
+        hashPopUp = Animator.StringToHash("popUp");
+        PopDown();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(nameof(PopUp_co));
     }
 
     private void LateUpdate()
@@ -27,5 +39,16 @@ public class FollowUI : MonoBehaviour
         Vector3 ScreenPosition = mainCamera.WorldToScreenPoint(target_obj.transform.position);
 
         UITrans.position = ScreenPosition + distance;
+    }
+
+    private IEnumerator PopUp_co()
+    {
+        anim.SetTrigger(hashPopUp);
+        yield return new WaitForSeconds(2f);
+        PopDown();
+    }
+    private void PopDown()
+    {
+        anim.Rebind();
     }
 }
