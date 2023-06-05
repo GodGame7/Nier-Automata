7585f658468737f67dd053616627bef3be73057d
VillageManA <sjthals@naver.com>
2023-06-05 10:07:06 +0900

    PordBullet 파티클시스템 최적화

--- Assets/3.Script/Bullet/PordBulletControl.cs
+++ Assets/3.Script/Bullet/PordBulletControl.cs
@@ -9,7 +9,7 @@ public class PordBulletControl : MonoBehaviour
     private void Awake()
     {
         Spark = Instantiate(Spark, transform.position, Quaternion.identity);
-
+        Spark.gameObject.SetActive(false);
     }
     private void OnEnable()
     {
@@ -22,24 +22,31 @@ public class PordBulletControl : MonoBehaviour
             other.transform.root.GetComponent<EnemyHp>().TakeDamage(6);
             transform.position = WaitLocation;
             gameObject.SetActive(false);
-            Spark.transform.position = other.gameObject.transform.position;
+
+            Spark.gameObject.transform.position = other.gameObject.transform.position;
+            Spark.gameObject.SetActive(true);
             Spark.Play();
+            Invoke("DisableSpark", 1f);
         }
         if (other.CompareTag("BulletSoft"))
         {
             other.gameObject.SetActive(false);
             gameObject.SetActive(false);
         }
-         
+
 
     }
-    
+
     private IEnumerator Disable() //불렛 지속시간 설정
     {
         yield return new WaitForSeconds(4f);
         gameObject.SetActive(false);
     }
-        
+
+    private void DisableSpark()
+    {
+        Spark.gameObject.SetActive(false);
+    }
 
 
 }
